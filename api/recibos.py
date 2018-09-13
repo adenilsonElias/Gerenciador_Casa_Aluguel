e636a60345a4fa55cadbb5b3ce32e743ce6a5597
taciossbr <tacioss.br@gmail.com>
2018-09-13 17:51:24 -0300

    implementação da api de recibos

---
+++ api/recibos.py
@@ -0,0 +1,33 @@
+from jinja2 import Environment, FileSystemLoader
+from weasyprint import HTML
+
+def gera_recibo(nome, dia, mes, ano, valor):
+    """
+    nome:Str, dia:Str, mes:int, ano:Str, valor:float
+    """
+    meses = [
+        None,
+        'Janeiro',
+        'Fevereiro',
+        'Março',
+        'Abril',
+        'Maio',
+        'Junho',
+        'Julho',
+        'Agosto',
+        'Setembro',
+        'Outubro',
+        'Novembro',
+        'Dezembro'
+    ]
+    env = Environment(loader=FileSystemLoader('./templates'))
+    template = env.get_template('recibo.html')
+    html = template.render({
+        'nome': nome,
+        'valor': valor,
+        'dia': 23,
+        'mes': meses[mes],
+        'ano': ano,
+    })
+    print(html)
+    HTML(string=html).write_pdf('recibos/{}-{}.pdf'.format(nome.replace(' ', '_'), meses[mes]))
