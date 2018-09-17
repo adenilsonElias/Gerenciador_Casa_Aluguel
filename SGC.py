782d0957cc3bf035d8bbcef7a4156c24ac1a134d
Adenilson Elias Da Silva <adenilson_elias@hotmail.com>
2018-09-17 20:55:34 -0300

    retorno do SGC

---
+++ SGC.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.insert(0, 'interface/')
+sys.path.insert(0, 'api/')
+from PyQt5.QtWidgets import QApplication
+from Menu import interagir_menu
+    
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = interagir_menu()
+    w.show()
+app.exec_()
